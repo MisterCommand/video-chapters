b6e7cef2d38a8a52d6559456b25f3b609116d38a
MisterCommand <anthony.tr.hou@gmail.com>
2024-11-01 10:47:54 +0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -11,7 +9,9 @@ const config = {
 
 	kit: {
 		appDir: 'app', // Required as the default is _app
-		adapter: adapter(),
+		adapter: adapter({
+            fallback: 'index.html'
+        }),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
